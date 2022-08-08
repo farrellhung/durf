783d4f69be0035d3793ac73d9660ed86b5a17439
Farrell Hung <91648049+farrellhung@users.noreply.github.com>
2022-08-08 23:56:05 +0800

    uses NIR video, added crop

--- cv.py
+++ cv.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 import cv2
 import numpy as np
 import math
@@ -13,15 +14,19 @@ def main():
     areaplot, = plt.plot([])
 
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (13, 13))
-    cap = cv2.VideoCapture('cam.mp4')
+    cap = cv2.VideoCapture('camnir.mp4')
+    # cap = cv2.VideoCapture('./nir/%02d.jpg')
 
     ret, raw = cap.read()
-    
+    size = (raw.shape[1], raw.shape[0]) # w*h
+    checkingrange = [[0,0],[size[0],size[1]]]
+    crop = raw.copy()
+
     while ret:
         cv2.imshow('raw', raw)
 
         # channel
-        B,G,R = cv2.split(raw)
+        B,G,R = cv2.split(crop)
         # cv2.imshow('channel', R)
         
         # morphology transformations
@@ -32,7 +37,7 @@ def main():
         cv2.imshow('closed', closed)
 
         # threshold
-        retval, closed = cv2.threshold(closed, 25, 255, cv2.ADAPTIVE_THRESH_MEAN_C)
+        retval, closed = cv2.threshold(closed, 130, 255, cv2.ADAPTIVE_THRESH_MEAN_C)
         cv2.imshow('threshold', closed)
 
         # contour
@@ -50,7 +55,7 @@ def main():
             circularity = area and circumference ** 2 / (4*math.pi*area)
 
             # reject some contours
-            if extend > 0.8 or area < 1000 or circularity > 1.1:
+            if extend > 0.8 or area < 200 or circularity > 1.05:
                 continue
 
             cv2.putText(raw, 'Area: ' + str(round(area, 2)), (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, 255, 2)
@@ -60,13 +65,14 @@ def main():
             # calculate countour center and draw a dot there
             m = cv2.moments(contour)
             if m['m00'] != 0:
-                center = (int(m['m10'] / m['m00']), int(m['m01'] / m['m00']))
+                center = (int(m['m10'] / m['m00'])+checkingrange[0][0], int(m['m01'] / m['m00'])+checkingrange[0][1])
                 cv2.circle(raw, center, 2, (255, 0, 0), -1)
+                checkingrange = [[int(center[0]-(size[0]/4)),int(center[1]-(size[1]/4))],[int(center[0]+(size[0]/4)),int(center[1]+(size[1]/4))]]
 
             # fit an ellipse around the contour and draw it into the image
             try:
-                c, r = cv2.minEnclosingCircle(contour)
-                ellipse = cv2.fitEllipse(contour)
+                ellipse = list(cv2.fitEllipse(contour))
+                ellipse[0] = [ellipse[0][0]+checkingrange[0][0],ellipse[0][1]+checkingrange[0][1]]
                 cv2.ellipse(raw, box=ellipse, color=(255, 0, 0))
             except Exception as e:
                 print(e)
@@ -77,11 +83,13 @@ def main():
             areavec.append(0)
 
         areaplot.set_data(range(len(areavec[-200:])),areavec[-200:])
-
         plt.draw()
         plt.pause(0.00001)
-        cv2.imshow('contour ellipse', raw)
+
+        cv2.imshow('result', raw)
         ret, raw = cap.read()
+
+        crop = raw[checkingrange[0][1]:checkingrange[1][1],checkingrange[0][0]:checkingrange[1][0]]
 
         if cv2.waitKey(10) & 0xff == ord(' '):          # press spacebar to pause/play
             if cv2.waitKey(0) & 0xff == ord('q'):       # press q after pausing to quit
